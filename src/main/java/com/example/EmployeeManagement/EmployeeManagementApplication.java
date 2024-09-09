41527ab6dff363f8c89be1804f357ba33a381a70
Kartik K <krkhengate123@gmail.com>
2024-09-09 22:15:02 +0530

    second welcome msg added

--- src/main/java/com/example/EmployeeManagement/EmployeeManagementApplication.java
+++ src/main/java/com/example/EmployeeManagement/EmployeeManagementApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class EmployeeManagementApplication {
 
 	public static void main(String[] args) {
+		System.out.println("welcome msg added in feature branch");
 		SpringApplication.run(EmployeeManagementApplication.class, args);
 	}
 
