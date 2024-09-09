dc2783556e9f96a5702386fb8171d474b5350c8e
Kartik K <krkhengate123@gmail.com>
2024-09-09 22:13:25 +0530

    welcome msg added

--- src/main/java/com/example/EmployeeManagement/EmployeeManagementApplication.java
+++ src/main/java/com/example/EmployeeManagement/EmployeeManagementApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class EmployeeManagementApplication {
 
 	public static void main(String[] args) {
+		System.out.println("welcome");
 		SpringApplication.run(EmployeeManagementApplication.class, args);
 	}
 
